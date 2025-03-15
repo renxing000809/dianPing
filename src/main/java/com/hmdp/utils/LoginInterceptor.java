ec5512fe07a9e8033b7abb9a685cc9cccaffd449
renxing <373006316@qq.com>
2025-03-15 18:54:09 +0800

    登录校验功能,个人信息接口开发

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,35 @@
+package com.hmdp.utils;
+
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 获取session
+        HttpSession session = request.getSession();
+        // 获取session中的用户
+        Object user = session.getAttribute("user");
+        // 判断用户是否存在
+        if (user == null) {
+            response.setStatus(401);
+            return false;
+        }
+        UserDTO userDTO = new UserDTO();
+        BeanUtils.copyProperties(user, userDTO);
+        UserHolder.saveUser(userDTO);
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
