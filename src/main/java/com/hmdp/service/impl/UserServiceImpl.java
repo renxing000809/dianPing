e941e14bdce0676b8edaab4eb2ebfeab0eae0f41
renxing <373006316@qq.com>
2025-03-15 16:12:50 +0800

    上传手机号、验证码实现注册功能

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -11,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -42,5 +45,39 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         log.debug("发送短信验证码成功，验证码：{}", code);
         // 返回ok
         return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 校验手机号和验证码
+        if (RegexUtils.isPhoneInvalid(loginForm.getPhone()) || !isCodeInvalid(loginForm, session)) {
+            // 2.如果不符合，返回错误信息
+            return Result.fail("手机号格式或验证码错误！");
+        }
+
+        // 一致，根据手机号查询用户
+        User user = query().eq("phone", loginForm.getPhone()).one();
+        // 判断用户是否存在
+        if (user == null) {
+            // 用户不存在，创建新用户，保存用户到数据库
+            user = createUserWithPhone(loginForm.getPhone());
+        }
+        // 保存用户到session
+        session.setAttribute("user", user);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
+    }
+
+    private boolean isCodeInvalid(LoginFormDTO loginForm, HttpSession session) {
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        return cacheCode != null && cacheCode.toString().equals(code);
     }
 }
